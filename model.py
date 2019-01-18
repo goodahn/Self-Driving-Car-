ba20206c70918c00b2bb2cba6439901d0bea05bd
goodahn <wnalj1589@gmail.com>
2019-01-19 05:38:31 +0900

    2nd trial of applying multi gpu in TF manner

--- model.py
+++ model.py
@@ -12,80 +12,86 @@ def bias_variable(shape):
 def conv2d(x, W, stride):
   return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='VALID')
 
+'''
 x = tf.placeholder(tf.float32, shape=[None, 66, 200, 3])
 y_ = tf.placeholder(tf.float32, shape=[None, 1])
 
 x_image = x
+'''
 
+def get_model(x):
+    #global x_image
+    x_image = x
 #first convolutional layer
-W_conv1 = weight_variable([5, 5, 3, 24])
-b_conv1 = bias_variable([24])
+    W_conv1 = weight_variable([5, 5, 3, 24])
+    b_conv1 = bias_variable([24])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1, 2) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1, 2) + b_conv1)
 
 #second convolutional layer
-W_conv2 = weight_variable([5, 5, 24, 36])
-b_conv2 = bias_variable([36])
+    W_conv2 = weight_variable([5, 5, 24, 36])
+    b_conv2 = bias_variable([36])
 
-h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
 
 #third convolutional layer
-W_conv3 = weight_variable([5, 5, 36, 48])
-b_conv3 = bias_variable([48])
+    W_conv3 = weight_variable([5, 5, 36, 48])
+    b_conv3 = bias_variable([48])
 
-h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 2) + b_conv3)
+    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 2) + b_conv3)
 
 #fourth convolutional layer
-W_conv4 = weight_variable([3, 3, 48, 64])
-b_conv4 = bias_variable([64])
+    W_conv4 = weight_variable([3, 3, 48, 64])
+    b_conv4 = bias_variable([64])
 
-h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, 1) + b_conv4)
+    h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, 1) + b_conv4)
 
 #fifth convolutional layer
-W_conv5 = weight_variable([3, 3, 64, 64])
-b_conv5 = bias_variable([64])
+    W_conv5 = weight_variable([3, 3, 64, 64])
+    b_conv5 = bias_variable([64])
 
-h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, 1) + b_conv5)
+    h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, 1) + b_conv5)
 
 #FCL 1
-W_fc1 = weight_variable([1152, 1164])
-b_fc1 = bias_variable([1164])
+    W_fc1 = weight_variable([1152, 1164])
+    b_fc1 = bias_variable([1164])
 
-h_conv5_flat = tf.reshape(h_conv5, [-1, 1152])
-h_fc1 = tf.nn.relu(tf.matmul(h_conv5_flat, W_fc1) + b_fc1)
+    h_conv5_flat = tf.reshape(h_conv5, [-1, 1152])
+    h_fc1 = tf.nn.relu(tf.matmul(h_conv5_flat, W_fc1) + b_fc1)
 
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    keep_prob = tf.Variable(0.8)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 #FCL 2
-W_fc2 = weight_variable([1164, 100])
-b_fc2 = bias_variable([100])
+    W_fc2 = weight_variable([1164, 100])
+    b_fc2 = bias_variable([100])
 
-h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
-
-#FCL 3
-W_fc3 = weight_variable([100, 50])
-b_fc3 = bias_variable([50])
-
-h_fc3 = tf.nn.relu(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
-
-h_fc3_drop = tf.nn.dropout(h_fc3, keep_prob)
+    h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
 #FCL 3
-W_fc4 = weight_variable([50, 10])
-b_fc4 = bias_variable([10])
+    W_fc3 = weight_variable([100, 50])
+    b_fc3 = bias_variable([50])
 
-h_fc4 = tf.nn.relu(tf.matmul(h_fc3_drop, W_fc4) + b_fc4)
+    h_fc3 = tf.nn.relu(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
 
-h_fc4_drop = tf.nn.dropout(h_fc4, keep_prob)
+    h_fc3_drop = tf.nn.dropout(h_fc3, keep_prob)
+
+#FCL 3
+    W_fc4 = weight_variable([50, 10])
+    b_fc4 = bias_variable([10])
+
+    h_fc4 = tf.nn.relu(tf.matmul(h_fc3_drop, W_fc4) + b_fc4)
+
+    h_fc4_drop = tf.nn.dropout(h_fc4, keep_prob)
 
 #Output
-W_fc5 = weight_variable([10, 1])
-b_fc5 = bias_variable([1])
+    W_fc5 = weight_variable([10, 1])
+    b_fc5 = bias_variable([1])
 
 #linear
 #y = tf.multiply((tf.matmul(h_fc4_drop, W_fc5) + b_fc5), 2)  
 ## atan
-y =  tf.multiply(tf.atan(tf.matmul(h_fc4_drop, W_fc5) + b_fc5), 2)#scale the atan output
+    y =  tf.multiply(tf.atan(tf.matmul(h_fc4_drop, W_fc5) + b_fc5), 2)#scale the atan output
+    return y
--- train.py
+++ train.py
@@ -5,6 +5,40 @@ import driving_data
 import model
 import time
 
+def read_and_decode(filename_queue):
+    
+    reader = tf.TFRecordReader()
+
+    _, serialized_example = reader.read(filename_queue)
+
+    features = tf.parse_single_example(
+      serialized_example,
+      # Defaults are not specified since both keys are required.
+      features={
+        'height': tf.FixedLenFeature([], tf.int64),
+        'width': tf.FixedLenFeature([], tf.int64),
+        'image_raw': tf.FixedLenFeature([], tf.string),
+        'mask_raw': tf.FixedLenFeature([], tf.string)
+        })
+
+    # Convert from a scalar string tensor (whose single string has
+    # length mnist.IMAGE_PIXELS) to a uint8 tensor with shape
+    # [mnist.IMAGE_PIXELS].
+    image = tf.decode_raw(features['image_raw'], tf.float32) / 255
+    annotation = tf.decode_raw(features['mask_raw'], tf.float32)
+    
+    height = tf.cast(features['height'], tf.int32)
+    width = tf.cast(features['width'], tf.int32)
+    
+    image_shape = tf.stack([tf.shape(image)[0], 66, 200, 3])
+    annotation_shape = tf.stack([tf.shape(image)[0], 1])
+    
+    print([tf.size(image), height, width, 3])
+    image = tf.reshape(image, image_shape)
+    annotation = tf.reshape(annotation, annotation_shape)
+    
+    return image, annotation
+
 def average_gradients(tower_grads):
     """Calculate the average gradient for each shared variable across all towers.
     Note that this function provides a synchronization point across all towers.
@@ -17,27 +51,28 @@ def average_gradients(tower_grads):
        across all towers.
     """
     average_grads = []
-    for grad_and_vars in zip(*tower_grads):
-        # Note that each grad_and_vars looks like the following:
-        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
-        grads = []
-        for g, _ in grad_and_vars:
-          # Add 0 dimension to the gradients to represent the tower.
-          expanded_g = tf.expand_dims(g, 0)
-       
-          # Append on a 'tower' dimension which we will average over below.
-          grads.append(expanded_g)
-       
-        # Average over the 'tower' dimension.
-        grad = tf.concat(axis=0, values=grads)
-        grad = tf.reduce_mean(grad, 0)
-       
-        # Keep in mind that the Variables are redundant because they are shared
-        # across towers. So .. we will just return the first tower's pointer to
-        # the Variable.
-        v = grad_and_vars[0][1]
-        grad_and_var = (grad, v)
-        average_grads.append(grad_and_var)
+    with tf.name_scope('gradient_averaging'):
+        for grad_and_vars in zip(*tower_grads):
+            # Note that each grad_and_vars looks like the following:
+            #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+            grads = []
+            for g, _ in grad_and_vars:
+              # Add 0 dimension to the gradients to represent the tower.
+              expanded_g = tf.expand_dims(g, 0)
+           
+              # Append on a 'tower' dimension which we will average over below.
+              grads.append(expanded_g)
+           
+            # Average over the 'tower' dimension.
+            grad = tf.concat(axis=0, values=grads)
+            grad = tf.reduce_mean(grad, 0)
+           
+            # Keep in mind that the Variables are redundant because they are shared
+            # across towers. So .. we will just return the first tower's pointer to
+            # the Variable.
+            v = grad_and_vars[0][1]
+            grad_and_var = (grad, v)
+            average_grads.append(grad_and_var)
     return average_grads
 
 
@@ -52,43 +87,63 @@ tf.app.flags.DEFINE_integer('num_gpus', 2,
 start = time.time()
 LOGDIR = './save'
 
-sess = tf.InteractiveSession()
+sess = tf.InteractiveSession(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
+#sess.run(tf.initialize_all_variables())
 
 L2NormConst = 0.001
 
 opt = tf.train.AdamOptimizer(1e-4)
 
-Xs, Ys = driving_data.LoadTrainBatch(int(driving_data.num_images))
-Xs = tf.convert_to_tensor(Xs)
-Ys = tf.convert_to_tensor(Ys)
+filename_queue = tf.train.string_input_producer(
+    ["driving_dataset/train_data.tfrecords"])
+Xs, Ys = read_and_decode(filename_queue)
+print(Xs.shape, Ys.shape)
+dataset = tf.data.Dataset.from_tensor_slices((Xs, Ys))
+'''
 Xs_batch, Ys_batch = tf.train.batch(
     [Xs, Ys],
     batch_size=batch_size,
-    num_threads=16,
-    capacity=0.4*driving_data.num_images+3*batch_size)
+    num_threads=16)
+    #capacity=0.4*driving_data.num_images+3*batch_size)
 Ys_batch_reshaped = tf.reshape(Ys_batch, [batch_size])
+'''
 
-batch_queue = tf.contrib.slim.prefetch_queue.prefetch_queue(
-        [Xs_batch, Ys_batch_reshaped], capacity=2*FLAGS.num_gpus)
 
 tower_grads = []
-with tf.variable_scope(tf.get_variable_scope()):
-    for i in xrange(FLAGS.num_gpus):
-        with tf.device("/gpu:{:d}".format(i)):
-            with tf.name_scope("train_{:d}".format(i)) as scope:
-                x_batch, y_batch = batch_queue.dequeue()
+for i in range(FLAGS.num_gpus):
+    print("{:d}th device".format(i))
+    with tf.variable_scope("cnn"):
+        with tf.name_scope("train_{:d}".format(i)) as scope:
+            with tf.device("/gpu:{:d}".format(i)):
+                shared_dataset = dataset.shard(FLAGS.num_gpus, i)
+                shared_dataset_batch = shared_dataset.batch(batch_size)
+                shared_dataset_batch.repeat(epochs)
+                iterator = shared_dataset_batch.make_initializable_iterator()
+                x_batch, y_batch = iterator.get_next()
+                '''
                 train_vars = tf.trainable_variables()
-                loss = tf.reduce_mean(tf.square(tf.subtract(model.y_, model.y))) + tf.add_n([tf.nn.l2_loss(v) for v in train_vars]) * L2NormConst
+                variables_names = [v.name for v in tf.trainable_variables()]
+                print("there are {:d} variables".format(len(variables_names)))
+                values = sess.run(variables_names)
+                for k, v in zip(variables_names, values):
+                    print( "Variable: ", k)
+                    print( "Shape: ", v.shape)
+                    print(v)
+                '''
+                loss = tf.reduce_mean(tf.square(tf.subtract(y_batch, model.get_model(x_batch)))) \
+                        + tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()]) * L2NormConst
                 tf.get_variable_scope().reuse_variables()
                 summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
-                grads = opt.compute_cradients(loss)
+                grads = opt.compute_gradients(loss)
                 tower_grads.append(grads)
 
 grads = average_gradients(tower_grads)
-apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+apply_gradient_op = opt.apply_gradients(grads)
 
+'''
 variable_averages = tf.train.ExponentialMovingAverage(0.9999, global_step)
-variables_averages_op = variable_averages.apply(tf.trainable_variables)
+variables_averages_op = variable_averages.apply(tf.trainable_variables())
+'''
 
 train_op = tf.group(apply_gradient_op, variables_averages_op)
 
@@ -101,7 +156,10 @@ for var in tf.trainable_variables():
 
 summary_op = tf.summary.merge(summaries)
 
+print("before run")
 sess.run(tf.initialize_all_variables())
+sess.run(iterator.initializer)
+print("after run")
 
 # create a summary to monitor cost tensor
 #tf.summary.scalar("loss", loss)
@@ -118,7 +176,7 @@ summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
 # train over the dataset about 30 times
 for epoch in range(epochs):
   for i in range(int(driving_data.num_images/batch_size)):
-    xs, ys = driving_data.LoadTrainBatch(batch_size)
+    #xs, ys = driving_data.LoadTrainBatch(batch_size)
     #train_step.run(feed_dict={model.x: xs, model.y_: ys, model.keep_prob: 0.8})
     _, loss_value = sess.run([train_op, loss])
 
